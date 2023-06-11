ef953831d74fae74b614ae53390a76c91b0c0e0e
Elizabeth Ogunmola <98459984+Elizzy01@users.noreply.github.com>
2023-06-11 16:41:08 +0100

    Completed the CRUD operation

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.homepage)
+    path("home/",views.homepage, name="home"),
+    path("deleteitem/<str:pk>/", views.deleteitem, name="deleteitem"),
+    path("update/<str:id>/", views.updateitem, name="update")
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import TodoApp
+
+
 
 # Create your views here.
 
@@ -16,3 +18,54 @@ def homepage(request):
             todoapp=TodoApp.objects.create(name=name)
     todoapp=TodoApp.objects.all()
     return render(request, "index.html", {"todolist":todoapp})
+
+def deleteitem(request, pk):
+    todoapp=TodoApp.objects.get(id=pk)
+    todoapp.delete()
+    print("Deleted")
+    todoapp=TodoApp.objects.all()
+
+    return redirect("home")
+
+
+def updateitem(request, id):  
+    todoapp = TodoApp.objects.get(id=id)
+    if request.method == "POST":
+        name = request.POST.get("name")
+        completed = request.POST.get("completed", False)
+        try:
+            completed = bool(completed)
+        except ValueError:
+            completed = False
+        if todoapp.completed == False and completed == True:
+            todoapp.completed = True
+
+        else:
+            todoapp.completed = False
+
+        todoapp.name = name
+        todoapp.save()
+        return redirect("home")
+    return render(request, "update.html", {"item": todoapp,"name":todoapp.name})
+
+
+
+def updateitem(request, pk):
+    todoapp = TodoApp.objects.get(id=pk)
+    if request.method == "POST":
+        name = request.POST.get("name")
+        completed = request.POST.get("completed", False)
+        try:
+            completed = bool(completed)
+        except ValueError:
+            completed = False
+
+        if todoapp.completed == False and completed == True:
+            todoapp.completed = True
+        else:
+            todoapp.completed = False
+        todoapp.name = name
+        todoapp.save()
+        return redirect("home")
+    return render(request, "update.html", {"item": todoapp,"name":todoapp.name})
+
