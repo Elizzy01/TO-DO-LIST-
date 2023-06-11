c2c537477e03f853bba04587223d638bfe082c77
Elizabeth Ogunmola <98459984+Elizzy01@users.noreply.github.com>
2023-06-11 15:21:37 +0100

    updated

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import TodoApp
 
 # Create your views here.
+
+def homepage(request):
+    if request.method=="POST":
+        name=request.POST["name"]
+        try:
+            completed=request.POST["completed"]
+        except:
+            completed=False
+        if completed == 'on':
+            todoapp=TodoApp.objects.create(name=name,completed=True)
+        else:
+            todoapp=TodoApp.objects.create(name=name)
+    todoapp=TodoApp.objects.all()
+    return render(request, "index.html", {"todolist":todoapp})
